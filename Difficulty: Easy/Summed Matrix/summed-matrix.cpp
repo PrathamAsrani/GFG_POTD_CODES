35d6ca46cb5340fc9e013e3d149ace47532661c8
Pratham <114286503+PrathamAsrani@users.noreply.github.com>
2024-06-24 15:38:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Summed Matrix/summed-matrix.cpp
@@ -0,0 +1,31 @@
+//{ Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    long long sumMatrix(long long n, long long q) {
+        long long low = 2, high = 2*n;
+        if(q < low || q > high) return 0;
+        return min(q-low, high-q) + 1;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long n,q;
+        
+        cin>>n>>q;
+
+        Solution ob;
+        cout << ob.sumMatrix(n,q) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
