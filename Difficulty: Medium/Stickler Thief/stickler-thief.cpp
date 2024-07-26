9bad91314acad89a90d089f1632a94eed98d78aa
Pratham <114286503+PrathamAsrani@users.noreply.github.com>
2024-07-26 21:49:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Stickler Thief/stickler-thief.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to find the maximum money the thief can get.
+    int FindMaxSum(int arr[], int n)
+    {
+        int one, two;
+        one = two = 0;
+        for(int i = 0; i < n; i++){
+            int tmp = max(one, two + arr[i]);
+            two = one;
+            one = tmp;
+        }
+        return one;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    //taking total testcases
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    //taking number of houses
+		int n;
+		cin>>n;
+		int a[n];
+		
+		//inserting money of each house in the array
+		for(int i=0;i<n;++i)
+			cin>>a[i];
+		Solution ob;
+		//calling function FindMaxSum()
+		cout<<ob.FindMaxSum(a,n)<<endl;
+	}
+	return 0;
+}
+
+// } Driver Code Ends
