225bd834c0ed1a6063d140310b758638ce76cec3
Pratham <114286503+PrathamAsrani@users.noreply.github.com>
2024-07-28 11:48:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Remove Duplicates/remove-duplicates.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+
+    string removeDups(string str) {
+        vector<int> dp(26, 0);
+        for(char &c: str)
+            dp[c-'a']++;
+        string ans;
+        for(char c: str){
+            if(dp[c-'a'] > 0){
+                dp[c-'a'] = 0;
+                ans += c;
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+
+        Solution ob;
+        cout << ob.removeDups(s) << "\n";
+    }
+
+    return 0;
+}
+// } Driver Code Ends
