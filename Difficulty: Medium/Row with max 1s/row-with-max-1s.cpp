71a43196980deaf252bbedcfad73b0ca9acc7d65
Pratham <114286503+PrathamAsrani@users.noreply.github.com>
2024-07-29 17:38:21 +0530

    Updated Solution - GfG to GitHub

---
+++ Difficulty: Medium/Row with max 1s/row-with-max-1s.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+    int rowWithMax1s(vector<vector<int> > &arr) {
+        int ans = -1, val = 0, n = arr.size();
+        for(int i = 0; i < n; i++){
+            int sum = accumulate(arr[i].begin(), arr[i].end(), 0);
+            if(val < sum){
+                val = sum;
+                ans = i;
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        vector<vector<int> > arr(n, vector<int>(m));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                cin >> arr[i][j];
+            }
+        }
+        Solution ob;
+        auto ans = ob.rowWithMax1s(arr);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
