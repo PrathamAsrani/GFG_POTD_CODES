1b3afa7d1cfad320497095b0db75150e2d44d1c5
Pratham <114286503+PrathamAsrani@users.noreply.github.com>
2024-08-02 10:19:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Edit Distance/edit-distance.cpp
+++ Difficulty: Hard/Edit Distance/edit-distance.cpp
@@ -2,29 +2,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
 // } Driver Code Ends
 class Solution {
-    int n, m;
     vector<vector<int>> dp;
+    int n, m;
   public:
-    int f(string &s, string &t, int i = 0, int j = 0) {
+    int fun(string &s, string &t, int i = 0, int j = 0){
         if(i == n) return m-j;
         if(j == m) return n-i;
         if(dp[i][j] != -1) return dp[i][j];
-        
-        if(s[i] == t[j]) return dp[i][j] = f(s, t, i+1, j+1);
-        else{
-            return dp[i][j] = min({
-                f(s, t, i, j+1),
-                f(s, t, i+1, j),
-                f(s, t, i+1, j+1)
-            }) + 1;
-        }
+        if(s[i] == t[j])
+            return dp[i][j] = fun(s, t, i+1, j+1);
+        else 
+            return dp[i][j] = 1 + min({fun(s, t, i+1, j), fun(s, t, i, j+1), fun(s, t, i+1, j+1)});
     }
     int editDistance(string s, string t) {
         n = s.size(), m = t.size();
         dp.resize(n, vector<int>(m, -1));
-        return f(s, t);
+        return fun(s, t);
     }
 };
 
