f88365db7d8f6b093838d3bb0d256b71b897adf3
Pratham <114286503+PrathamAsrani@users.noreply.github.com>
2024-08-30 16:36:56 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/N-Queen Problem/nqueen-problem.cpp
+++ Difficulty: Hard/N-Queen Problem/nqueen-problem.cpp
@@ -8,56 +8,53 @@ using namespace std;
 // User function Template for C++
 
 class Solution{
-public:
-    void addSolution(int n, vector<vector<int>> &board, vector<vector<int>> &ans){
-        vector<int> temp;
-        for(int i = 0; i < n; i++) for(int j = 0; j < n; j++) if(board[i][j] == 1) temp.push_back(j+1);
-        ans.push_back(temp);
+    vector<vector<int>> ans;
+    void getBoard(vector<vector<int>> &board, int n){
+        vector<int> tmp;
+        for(int i = 0; i < n; i++) for(int j = 0; j < n; j++) if(board[i][j]) tmp.push_back(j+1);
+        ans.push_back(tmp);
     }
-    bool isSafe(int row, int col, int n, vector<vector<int>> &board){
-        for(int i = 0; i < n; i++) if(board[i][col] == 1) return false; // check row
-        for(int i = 0; i < n; i++) if(board[row][i] == 1) return false; // check col
-        int x = row, y = col;
-        while(x >= 0 && y >= 0){
-            if(board[x--][y--] == 1) return false; // check diagonal left up
-        }
-        x = row, y = col;
-        while(x < n && y < n){
-            if(board[x++][y++] == 1) return false; // check diagonal right down
-        }
-        x = row, y = col;
-        while(x >= 0 && y < n){
-            if(board[x--][y++] == 1) return false; // check diagonal left down
-        }
-        x = row, y = col;
-        while(x < n && y >= 0){
-            if(board[x++][y--] == 1) return false; // check diagonal right up
-        }
+    bool isSafe(vector<vector<int>> &board, int row, int col){
+        int n = board.size();
+        // check row
+        for(int i = 0; i < n; i++) if(board[row][i] == 1) return false;
+        // check col
+        for(int i = 0; i < n; i++) if(board[i][col] == 1) return false;
+        
+        // check diag left-up
+        for(int i = row, j = col; i >= 0 && j >= 0; i--, j--) if(board[i][j] == 1) return false;
+        // check diag left-down
+        for(int i = row, j = col; i < n && j < n; i++, j++) if(board[i][j] == 1) return false;
+        
+        // check diag right-up
+        for(int i = row, j = col; i < n && j >= 0; i++, j--) if(board[i][j] == 1) return false;
+        // check diag right-down
+        for(int i = row, j = col; i >= 0 && j < n; i--, j++) if(board[i][j] == 1) return false;
+        
         return true;
     }
-    void solve(int col, int n, vector<vector<int>> &board, vector<vector<int>> &ans){
-        if(col == n){
-            addSolution(n, board, ans);
+    void solve(int col, int n, vector<vector<int>> &board){
+        if(col == n) {
+            getBoard(board, n);
             return ;
         }
         for(int row = 0; row < n; row++){
-            if(isSafe(row, col, n, board) == true){
+            if(isSafe(board, row, col)){
                 board[row][col] = 1;
-                solve(col+1, n, board, ans);
+                solve(col+1, n, board);
                 board[row][col] = 0;
             }
         }
     }
-
+public:
     vector<vector<int>> nQueen(int n) {
-        vector<vector<int>> ans, board(n, vector<int>(n, 0));
-        solve(0, n, board, ans);
+        // code here
+        vector<vector<int>> board(n, vector<int>(n, 0));
+        solve(0, n, board);
         sort(ans.begin(), ans.end());
         return ans;
     }
 };
-
-
 
 //{ Driver Code Starts.
 
