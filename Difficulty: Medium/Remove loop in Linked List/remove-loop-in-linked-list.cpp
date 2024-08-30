c4d5e5af27d15dc7b4f6f28003f0dcbe9be85acc
Pratham <114286503+PrathamAsrani@users.noreply.github.com>
2024-08-30 22:14:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Remove loop in Linked List/remove-loop-in-linked-list.cpp
@@ -0,0 +1,169 @@
+//{ Driver Code Starts
+// driver code
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+
+    Node(int val) {
+        data = val;
+        next = NULL;
+    }
+};
+
+void loopHere(Node* head, Node* tail, int position) {
+    if (position == 0)
+        return;
+
+    Node* walk = head;
+    for (int i = 1; i < position; i++)
+        walk = walk->next;
+    tail->next = walk;
+}
+
+bool isLoop(Node* head) {
+    if (!head)
+        return false;
+
+    Node* fast = head->next;
+    Node* slow = head;
+
+    while (fast != slow) {
+        if (!fast || !fast->next)
+            return false;
+        fast = fast->next->next;
+        slow = slow->next;
+    }
+
+    return true;
+}
+
+int length(Node* head) {
+    int ret = 0;
+    while (head) {
+        ret++;
+        head = head->next;
+    }
+    return ret;
+}
+
+bool notOriginal(Node* head, unordered_map<Node*, int>& myMap) {
+
+    while (head) {
+        if (myMap.find(head) == myMap.end())
+            return true;
+        if (myMap[head] != (head->data))
+            return true;
+
+        head = head->next;
+    }
+}
+
+
+// } Driver Code Ends
+/*
+structure of linked list node:
+
+struct Node
+{
+    int data;
+    Node* next;
+
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+*/
+
+class Solution {
+  public:
+    // Function to remove a loop in the linked list.
+    void removeLoop(Node* head) {
+        // code here
+        // just remove the loop without losing any nodes
+        Node *s = head, *f = head;
+        s = s->next;
+        if(f->next) f = f->next->next;
+        while(f && f->next){
+            s = s->next;
+            f = f->next->next;
+            if(s == f) break;
+        }
+        if(s != f) return ;
+        // case 1 if s or f pointing to start
+        if(s == head){
+            while(f->next != s){
+                f = f->next;
+            }
+            f->next = nullptr;
+        } 
+        // case 2: not pointing the start
+        else {
+            s = head;
+            while(s->next != f->next){
+                f = f->next;
+                s = s->next;
+            }
+            f->next = nullptr;
+        }
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        unordered_map<Node*, int> myMap;
+
+        int n, num;
+        n = arr.size();
+
+        Node *head, *tail;
+        num = arr[0];
+        head = tail = new Node(num);
+
+        myMap[head] = num;
+
+        for (int i = 1; i < n; i++) {
+            num = arr[i];
+            tail->next = new Node(num);
+            tail = tail->next;
+            myMap[tail] = num;
+        }
+
+        int pos;
+        cin >> pos;
+        cin.ignore();
+        loopHere(head, tail, pos);
+
+        Solution ob;
+        ob.removeLoop(head);
+
+        if (isLoop(head) || length(head) != n || notOriginal(head, myMap))
+            cout << "false\n";
+        else
+            cout << "true\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
