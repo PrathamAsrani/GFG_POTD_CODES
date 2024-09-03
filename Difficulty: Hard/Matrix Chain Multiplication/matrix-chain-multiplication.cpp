749f7473637ef803b60cce4529983bed952fc093
Pratham <114286503+PrathamAsrani@users.noreply.github.com>
2024-09-03 19:41:27 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Matrix Chain Multiplication/matrix-chain-multiplication.cpp
+++ Difficulty: Hard/Matrix Chain Multiplication/matrix-chain-multiplication.cpp
@@ -31,11 +31,28 @@ public:
     int matrixMultiplication(int N, int arr[])
     {
         // code here
+        /*
         memset(dp, -1, sizeof(dp));
         // pass the full block
         return fun(1, N-1, arr);
+        */
+        
+        memset(dp, 0, sizeof(dp));
+        for(int i = 1; i < N; i++) dp[i][i] = 0;
+        for(int i = N-1; i > 0; i--){
+            for(int j = i+1; j < N; j++){
+                int multiplications = 1e9;
+                for(int k = i; k < j; k++){
+                    multiplications = min(multiplications, 
+                        (arr[i-1] * arr[k] * arr[j]) + dp[i][k] + dp[k+1][j]);
+                }
+                dp[i][j] = multiplications;
+            }
+        }
+        return dp[1][N-1];
     }
 };
+
 
 //{ Driver Code Starts.
 
