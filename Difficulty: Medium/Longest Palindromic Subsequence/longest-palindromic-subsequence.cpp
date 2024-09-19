2d6e9aaab215e8cd6c1922c889dbebd3106f1117
Pratham <114286503+PrathamAsrani@users.noreply.github.com>
2024-09-19 14:45:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Palindromic Subsequence/longest-palindromic-subsequence.cpp
+++ Difficulty: Medium/Longest Palindromic Subsequence/longest-palindromic-subsequence.cpp
@@ -22,9 +22,52 @@ class Solution{
         return dp[n][m];
     }
     int longestPalinSubseq(string A) {
+        /*
+        // bottom up
+        
         string B = A;
         reverse(B.begin(), B.end());
         return LCS(A, B);
+        */
+        
+        /*
+        string s = A;
+        int n = s.size();
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+        function<int(int, int)> td = [&](int i, int j) -> int {
+            if( i > j ) return 0;
+            if(dp[i][j] != -1) return dp[i][j];
+            int val = 0;
+
+            if(s[i] == s[j]){
+                if(i == j) val = 1 + td(i+1, j-1);
+                else val = 2 + td(i+1, j-1);
+            }else{
+                val = max(td(i+1, j), td(i, j-1));
+            }
+
+            return dp[i][j] = val;
+        };
+        return td(0, n-1);
+        */
+        
+        
+        string s = A;
+        int n = s.size();
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+        for(int i = n-1; i >= 0; i--){
+            dp[i][i] = 1;
+            for(int j = i+1; j < n; j++){
+                int val = 0;
+                if(s[i] == s[j]){
+                    val = 2 + dp[i+1][j-1];
+                } else {
+                    val = max(dp[i+1][j], dp[i][j-1]);
+                }
+                dp[i][j] = val;
+            }
+        }
+        return dp[0][n-1];
     }
 };
 
