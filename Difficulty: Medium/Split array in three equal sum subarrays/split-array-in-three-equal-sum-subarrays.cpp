3fb95017a8f258f472f55943dd2a1de9c39c8cc2
Pratham <114286503+PrathamAsrani@users.noreply.github.com>
2024-11-07 16:29:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Split array in three equal sum subarrays/split-array-in-three-equal-sum-subarrays.cpp
@@ -0,0 +1,88 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+//  Class Solution to contain the method for solving the problem.
+class Solution {
+    typedef long long ll;
+  public:
+    // Function to determine if array arr can be split into three equal sum sets.
+    vector<int> findSplit(vector<int>& arr) {
+        // code here
+        int n = arr.size();
+        ll sum = arr[0];
+        vector<ll> prefix(n, 0);
+        prefix[0] = arr[0];
+        for(int i = 1; i < n; i++){
+            sum += arr[i];
+            prefix[i] = sum;
+        }
+        
+        if(sum%3 != 0)
+            return {-1, -1};
+        ll k = sum/3;
+        int i = lower_bound(prefix.begin(), prefix.end(), k) - prefix.begin();
+        int j = i+1;
+        for(int idx = j; idx < n; idx++){
+            if(prefix[idx] == 2*k){
+                j = idx;
+                break;
+            }
+        }
+        return {i, j};
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int test_cases;
+    cin >> test_cases;
+    cin.ignore();
+    while (test_cases--) {
+        string input;
+        vector<int> arr;
+
+        // Read the array from input line
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        // Solution instance to invoke the function
+        Solution ob;
+        vector<int> result = ob.findSplit(arr);
+
+        // Output result
+        if (result[0] == -1 && result[1] == -1 || result.size() != 2) {
+            cout << "false" << endl;
+        } else {
+            int sum1 = 0, sum2 = 0, sum3 = 0;
+            for (int i = 0; i < arr.size(); i++) {
+                if (i <= result[0])
+                    sum1 += arr[i];
+
+                else if (i <= result[1])
+                    sum2 += arr[i];
+
+                else
+                    sum3 += arr[i];
+            }
+            if (sum1 == sum2 && sum2 == sum3) {
+                cout << "true" << endl;
+            } else {
+                cout << "false" << endl;
+            }
+        }
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
