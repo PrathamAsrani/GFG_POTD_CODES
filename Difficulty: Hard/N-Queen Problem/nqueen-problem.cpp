cde55a23c234f849f7cf205ad6b787daba84c3cd
Pratham <114286503+PrathamAsrani@users.noreply.github.com>
2025-01-30 22:12:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/N-Queen Problem/nqueen-problem.cpp
+++ Difficulty: Hard/N-Queen Problem/nqueen-problem.cpp
@@ -4,80 +4,67 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
 // } Driver Code Ends
 // User function Template for C++
 
-class Solution{
+class Solution {
     vector<vector<int>> ans;
-    void getBoard(vector<vector<int>> &board, int n){
-        vector<int> tmp;
-        for(int i = 0; i < n; i++) for(int j = 0; j < n; j++) if(board[i][j]) tmp.push_back(j+1);
-        ans.push_back(tmp);
-    }
-    bool isSafe(vector<vector<int>> &board, int row, int col){
-        int n = board.size();
-        // check row
-        for(int i = 0; i < n; i++) if(board[row][i] == 1) return false;
-        // check col
-        for(int i = 0; i < n; i++) if(board[i][col] == 1) return false;
-        
-        // check diag left-up
-        for(int i = row, j = col; i >= 0 && j >= 0; i--, j--) if(board[i][j] == 1) return false;
-        // check diag left-down
-        for(int i = row, j = col; i < n && j < n; i++, j++) if(board[i][j] == 1) return false;
-        
-        // check diag right-up
-        for(int i = row, j = col; i < n && j >= 0; i++, j--) if(board[i][j] == 1) return false;
-        // check diag right-down
-        for(int i = row, j = col; i >= 0 && j < n; i--, j++) if(board[i][j] == 1) return false;
-        
-        return true;
-    }
-    void solve(int col, int n, vector<vector<int>> &board){
-        if(col == n) {
-            getBoard(board, n);
+    void solve(int col, int &n, vector<int> &placed, vector<bool> &rows, vector<bool> &upperDiag, vector<bool> &lowerDiag){
+        if(col == n){
+            ans.push_back(placed);
             return ;
         }
         for(int row = 0; row < n; row++){
-            if(isSafe(board, row, col)){
-                board[row][col] = 1;
-                solve(col+1, n, board);
-                board[row][col] = 0;
+            if(!rows[row] && !upperDiag[col-row+n-1] && !lowerDiag[col+row]){
+                rows[row] = upperDiag[col-row+n-1] = lowerDiag[row+col] = 1;
+                placed.push_back(row+1);
+                solve(col+1, n, placed, rows, upperDiag, lowerDiag);
+                rows[row] = upperDiag[col-row+n-1] = lowerDiag[row+col] = 0;
+                placed.pop_back();
             }
         }
     }
-public:
+  public:
     vector<vector<int>> nQueen(int n) {
         // code here
-        vector<vector<int>> board(n, vector<int>(n, 0));
-        solve(0, n, board);
+        vector<bool> row(n, false), upperDiag(2*n-1, false), lowerDiag(2*n-1, 0);
+        vector<int> placed;
+        solve(0, n, placed, row, upperDiag, lowerDiag);
         sort(ans.begin(), ans.end());
         return ans;
     }
 };
 
+
+
+
 //{ Driver Code Starts.
 
-int main(){
+int main() {
     int t;
-    cin>>t;
-    while(t--){
+    cin >> t;
+    while (t--) {
         int n;
-        cin>>n;
-        
+        cin >> n;
+
         Solution ob;
         vector<vector<int>> ans = ob.nQueen(n);
-        if(ans.size() == 0)
-            cout<<-1<<"\n";
+        if (ans.size() == 0)
+            cout << -1 << "\n";
         else {
-            for(int i = 0;i < ans.size();i++){
-                cout<<"[";
-                for(int u: ans[i])
-                    cout<<u<<" ";
-                cout<<"] ";
+            sort(ans.begin(), ans.end());
+            for (int i = 0; i < ans.size(); i++) {
+                cout << "[";
+                for (int u : ans[i])
+                    cout << u << " ";
+                cout << "] ";
             }
-            cout<<endl;
+            cout << endl;
         }
+
+        cout << "~"
+             << "\n";
     }
     return 0;
 }
