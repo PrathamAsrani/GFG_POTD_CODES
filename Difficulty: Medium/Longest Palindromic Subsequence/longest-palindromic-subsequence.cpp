ed7c5b29173611b695e0f3b9ea0e63b928b82125
Pratham <114286503+PrathamAsrani@users.noreply.github.com>
2025-03-07 10:11:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Palindromic Subsequence/longest-palindromic-subsequence.cpp
+++ Difficulty: Medium/Longest Palindromic Subsequence/longest-palindromic-subsequence.cpp
@@ -1,89 +1,52 @@
 //{ Driver Code Starts
-//Initial template for C++
+// Initial template for C++
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 
 // } Driver Code Ends
-//User function Template for C++
 
-class Solution{
-  public:
-    int LCS(string a, string b){
-        int n = a.size(), m = b.size();
-        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
-        for(int i = 1; i <= n; i++){
-            for(int j = 1; j <= m; j++){
-                if(a[i-1] == b[j-1]) dp[i][j] = 1+dp[i-1][j-1];
-                else dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
-            }
+// User function Template for C++
+
+class Solution {
+    vector<vector<int>> dp;
+    int fun(string &s, int i, int j){
+        if(i > j)
+            return 0;
+        else if(dp[i][j] != -1)
+            return dp[i][j];
+            
+        if(s[i] == s[j]){
+            return dp[i][j] = (i == j) ? 1 + fun(s, i+1, j-1) : 2 + fun(s, i+1, j-1);
         }
-        return dp[n][m];
+        else {
+            return dp[i][j] = max(fun(s, i+1, j), fun(s, i, j-1));
+        }
     }
-    int longestPalinSubseq(string A) {
-        /*
-        // bottom up
-        
-        string B = A;
-        reverse(B.begin(), B.end());
-        return LCS(A, B);
-        */
-        
-        /*
-        string s = A;
-        int n = s.size();
-        vector<vector<int>> dp(n, vector<int>(n, -1));
-        function<int(int, int)> td = [&](int i, int j) -> int {
-            if( i > j ) return 0;
-            if(dp[i][j] != -1) return dp[i][j];
-            int val = 0;
-
-            if(s[i] == s[j]){
-                if(i == j) val = 1 + td(i+1, j-1);
-                else val = 2 + td(i+1, j-1);
-            }else{
-                val = max(td(i+1, j), td(i, j-1));
-            }
-
-            return dp[i][j] = val;
-        };
-        return td(0, n-1);
-        */
-        
-        
-        string s = A;
-        int n = s.size();
-        vector<vector<int>> dp(n, vector<int>(n, 0));
-        for(int i = n-1; i >= 0; i--){
-            dp[i][i] = 1;
-            for(int j = i+1; j < n; j++){
-                int val = 0;
-                if(s[i] == s[j]){
-                    val = 2 + dp[i+1][j-1];
-                } else {
-                    val = max(dp[i+1][j], dp[i][j-1]);
-                }
-                dp[i][j] = val;
-            }
-        }
-        return dp[0][n-1];
+  public:
+    int longestPalinSubseq(string &s) {
+        // code here
+        dp.resize(s.size(), vector<int>(s.size(), -1));
+        return fun(s, 0, s.size()-1);
     }
 };
 
 
 //{ Driver Code Starts.
 
-int32_t main()
-{
-    int t; cin >> t;
-    while (t--)
-    {
-        string s; cin >> s;
+int32_t main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
         Solution ob;
         cout << ob.longestPalinSubseq(s) << endl;
+
+        cout << "~"
+             << "\n";
     }
 }
-// Contributed By: Pranay Bansal
 
 // } Driver Code Ends
