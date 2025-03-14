25d28e3adc35f361717c13ef89c4def1877b7a93
Pratham <114286503+PrathamAsrani@users.noreply.github.com>
2025-03-14 13:30:26 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Coin Change (Count Ways)/coin-change-count-ways.cpp
+++ Difficulty: Medium/Coin Change (Count Ways)/coin-change-count-ways.cpp
@@ -2,43 +2,64 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
 // } Driver Code Ends
+
 class Solution {
-    typedef long long int lli;
-  public:
-    long long int count(int arr[], int n, int sum) {
-        vector<lli> dp(sum+1, 0);
-        dp[0] = 1;
-        for(int i = n-1; i >= 0; i--){
-            vector<lli> curr(sum+1, 0);
-            curr[0] = 1;
-            for(int j = 0; j <= sum; j++){
-                lli notTake = dp[j], take = 0;
-                if(j >= arr[i])
-                    take = curr[j-arr[i]];
-                curr[j] = take + notTake;
-            }
-            dp = curr;
+    int n;
+    vector<vector<int>> dp;
+    int fun(vector<int> &coins, int sum, int idx = 0){
+        if(idx >= n)
+            return sum == 0 ? 1 : 0;
+        else if(sum == 0)
+            return 1;
+        else if(sum < 0)
+            return 0;
+        else if(dp[idx][sum] != -1)
+            return dp[idx][sum];
+        
+        int take = 0, notTake = fun(coins, sum, idx+1);
+        if(sum >= coins[idx]){
+            take = fun(coins, sum - coins[idx], idx);
         }
-        return dp[sum];
+        
+        return dp[idx][sum] = take + notTake;
+    }
+  public:
+    int count(vector<int>& coins, int sum) {
+        // code here.
+        n = coins.size();
+        dp.resize(n, vector<int>(sum + 1, -1));
+        return fun(coins, sum);
     }
 };
 
+
 //{ Driver Code Starts.
+
 int main() {
+
     int t;
     cin >> t;
+    cin.ignore();
     while (t--) {
-        int sum, N;
-        cin >> sum >> N;
-        int coins[N];
-        for (int i = 0; i < N; i++) cin >> coins[i];
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        int sum;
+        cin >> sum;
+        cin.ignore();
         Solution ob;
-        cout << ob.count(coins, N, sum) << endl;
+        cout << ob.count(arr, sum) << endl;
+        cout << "~" << endl;
     }
 
     return 0;
 }
-
 
 // } Driver Code Ends
