201def1685383d90f4360f1b811528d81a72b2c3
Pratham <114286503+PrathamAsrani@users.noreply.github.com>
2025-03-21 10:22:06 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Stickler Thief/stickler-thief.cpp
+++ Difficulty: Medium/Stickler Thief/stickler-thief.cpp
@@ -3,45 +3,46 @@
 using namespace std;
 typedef long long int ll;
 
+
 // } Driver Code Ends
-class Solution
-{
-    public:
-    //Function to find the maximum money the thief can get.
-    int FindMaxSum(int arr[], int n)
-    {
-        int one, two;
-        one = two = 0;
+
+class Solution {
+  public:
+    int findMaxSum(vector<int>& arr) {
+        // code here
+        int n = arr.size();
+        int one = 0, two = 0;
         for(int i = 0; i < n; i++){
             int tmp = max(one, two + arr[i]);
             two = one;
             one = tmp;
         }
-        return one;
+        return max(one, two);
     }
 };
 
+
 //{ Driver Code Starts.
-int main()
-{
-    //taking total testcases
-	int t;
-	cin>>t;
-	while(t--)
-	{
-	    //taking number of houses
-		int n;
-		cin>>n;
-		int a[n];
-		
-		//inserting money of each house in the array
-		for(int i=0;i<n;++i)
-			cin>>a[i];
-		Solution ob;
-		//calling function FindMaxSum()
-		cout<<ob.FindMaxSum(a,n)<<endl;
-	}
-	return 0;
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        cout << ob.findMaxSum(arr) << endl;
+        cout << "~" << endl;
+    }
+    return 0;
 }
 
 // } Driver Code Ends
